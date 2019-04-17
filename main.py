52ed1c5e208eafea7e39566caecd873208048c6c
AndreaLiboni <liboni.andrea@gmail.com>
2019-04-17 09:49:25 +0200

    ciao

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import xml.etree as xml
+
+from bin.Lesson import Lesson
+
+
+def getParam(tag):
+    if tag == "link"
+        return tree.find(tag).text
+
+
+f = open('lesson2.xml', 'r')
+tree = xml.ElementTree.parse(f)
+
 if __name__ == "__main__":
-    print("Hello world!")
+    l = Lesson()
+    tags = ["title", "description", "text", "link"]
+    for i in tags:
+        print(getParam(i))

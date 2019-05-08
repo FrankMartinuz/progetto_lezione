f97f1400c05d3066cc2c68c18d26fdf8793e9d09
BryanImaralu <Bryan.ima01@gmail.com>
2019-05-08 11:30:34 +0200

    Teacher class

--- bin/teacher.py
+++ bin/teacher.py
@@ -1,5 +1,7 @@
-# from bin.user import Lesson
+from bin.controller import Controller
+from bin.lesson import Lesson
 from bin.user import User
+from bin.view_subject import View_subject
 
 
 class Teacher(User):
@@ -8,8 +10,16 @@ class Teacher(User):
         self.__subject = None
         self.__id_teacher = None
 
-    def chooseSubject(self, subject):
-        self.__subject = subject
+    def viewSubject(self, entry_first_name, entry_last_name, controller):
+        self.set_first_name(entry_first_name.get())
+        self.set_last_name(entry_last_name.get())
+
+        "stop della finestra di login"
+        controller.stop()
+
+        "istanza un nuovo controller per aprire una nuova finestra"
+        controller_view_subject = Controller(View_subject)
+        controller_view_subject.run()
 
     def addLesson(self, idt, title, desc, text, link):
         lesson = Lesson(idt, title, desc, text, link)
@@ -18,13 +28,19 @@ class Teacher(User):
     def removeLesson(self):
         return
 
-    def addQuiz(self):
-        quiz = Quiz()
-        quiz.createQuiz()
-        return
+    # def addQuiz(self):
+    #     quiz = Quiz()
+    #     quiz.createQuiz()
+    #     return
 
     def removeQuiz(self):
         return
+
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+
+    def set_last_name(self, last_name):
+        self._last_name = last_name
 
 
 if __name__ == "__main__":
